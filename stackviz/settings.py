b94e378de71ff7192c366ad701c4959c4340abd9
Tim Buckley <timothy.jas.buckley@hp.com>
2015-07-14 10:26:59 -0600

    fix template and static file paths; create empty index page

--- stackviz/settings.py
+++ stackviz/settings.py
@@ -74,3 +74,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'stackviz', 'static')
+]
+
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'stackviz', 'templates')
+]
--- stackviz/urls.py
+++ stackviz/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from stackviz.views.index import IndexView
+
+import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'stackviz.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', IndexView.as_view()),
+
 )
