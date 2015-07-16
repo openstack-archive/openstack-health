46a2254b7405234ab77089acc4e72a229c076010
Tim Buckley <timothy.jas.buckley@hp.com>
2015-07-16 11:22:07 -0600

    add URL mappings for new API endpoints and timeline view

--- stackviz/views/tempest/urls.py
+++ stackviz/views/tempest/urls.py
@@ -1,11 +1,30 @@
 from django.conf.urls import patterns, include, url
 
 from .latest_results import LatestResultsView
-from .api import TempestRunEndpoint
+from .timeline import TimelineView, TimelineLatestView
+
+from .api import (TempestRunTreeEndpoint,
+                  TempestRunRawEndpoint,
+                  TempestRunDetailsEndpoint)
 
 
 urlpatterns = patterns('',
     url(r'^latest_results$', LatestResultsView.as_view()),
 
-    url(r'^api/(\d+)/$', TempestRunEndpoint.as_view())
+    url(r'^timeline/(?P<run_id>\d+)/$',
+        TimelineView.as_view(),
+        name='tempest_timeline'),
+    url(r'^timeline/$',
+        TimelineLatestView.as_view(),
+        name='tempest_timeline_latest'),
+
+    url(r'^api/tree/(?P<run_id>\d+)/$',
+        TempestRunTreeEndpoint.as_view(),
+        name='tempest_api_tree'),
+    url(r'^api/raw/(?P<run_id>\d+)/$',
+        TempestRunRawEndpoint.as_view(),
+        name='tempest_api_raw'),
+    url(r'^api/details/(\d+)/([^/]+)/$',
+        TempestRunDetailsEndpoint.as_view(),
+        name='tempest_api_details'),
 )
