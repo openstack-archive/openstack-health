def89936d6f82a21e692cfcc8fc35a0c5c66402f
Tim Buckley <timothy.jas.buckley@hp.com>
2015-07-30 10:43:49 -0600

    Add 'tempest_runs' variable to templates and add run selector menu to timeline

--- stackviz/global_template_injector.py
+++ stackviz/global_template_injector.py
@@ -4,5 +4,6 @@ from stackviz.settings import USE_GZIP
 def inject_extra_context(request):
     return {
         'tempest_latest_run': get_repositories()[0].latest_id(),
+        'tempest_runs': xrange(get_repositories()[0].count()),
         'use_gzip': USE_GZIP
     }
