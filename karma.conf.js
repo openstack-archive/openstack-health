7c66f0d74920089e43dd549cb373784706748ba0
Tim Buckley <timothy.jas.buckley@hp.com>
2015-08-03 15:16:26 -0600

    Minor adjustments to karma config and npm packages

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function(config) {
     config.set({
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: 'stackviz/static/js/',
+        basePath: 'stackviz/static/',
 
 
         // frameworks to use
@@ -14,7 +14,13 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-          '**/*.js'
+            'components/jquery/dist/jquery.js',
+            'components/datatables/media/js/jquery.dataTables.js',
+            'components/bootstrap/dist/js/bootstrap.js',
+            'components/metisMenu/dist/metisMenu.js',
+            'components/d3/d3.js',
+            'js/**/*.js',
+            'tests/**/*.js'
         ],
 
 
