63233b3461a680b053acfdd7ef266767aaa50563
Glauco Oliveira <gvinici@thoughtworks.com>
2015-10-06 15:32:40 -0300

    Add live data on second page
    This adds live data laod to second page (jobs by project).
    
    It was necessary to change the way that project name param was catched
    in url because project names contains slash.
    
    Change-Id: I233f878bcdee212688aadefb8fb6b7d1dfa21af0

--- app/js/controllers/project.js
+++ app/js/controllers/project.js
@@ -5,26 +5,28 @@ var controllersModule = require('./_index');
 /**
  * @ngInject
  */
-function ProjectCtrl($http, projectName) {
+function ProjectCtrl($http, healthService, projectName) {
 
   // ViewModel
   var vm = this;
 
   vm.name = projectName;
 
-  $http.get("data/project_sample.json").then(function(response) {
-    vm.data = response.data;
-
+  vm.processData = function(data) {
     // prepare chart data
     var jobs = {};
     var passEntries = [];
     var failEntries = [];
 
-    response.data.timedata.forEach(function(timedata) {
+    if (!data.timedelta) {
+      return;
+    }
+
+    data.timedelta.forEach(function(timedelta) {
       var totalPass = 0;
       var totalFail = 0;
 
-      timedata.job_data.forEach(function(job) {
+      timedelta.job_data.forEach(function(job) {
         if (!jobs[job.job_name]) {
           var job_metrics = {
             name: job.job_name,
@@ -48,12 +50,12 @@ function ProjectCtrl($http, projectName) {
       });
 
       passEntries.push({
-        x: new Date(timedata.datetime).getTime(),
+        x: new Date(timedelta.datetime).getTime(),
         y: totalPass
       });
 
       failEntries.push({
-        x: new Date(timedata.datetime).getTime(),
+        x: new Date(timedelta.datetime).getTime(),
         y: totalFail
       });
     });
@@ -64,7 +66,21 @@ function ProjectCtrl($http, projectName) {
     ];
 
     vm.jobs = jobs;
-  });
+  }
+
+  vm.loadData = function() {
+    var start = new Date();
+    start.setDate(start.getDate() - 20);
+
+    healthService.getRunsFromProject(projectName, {
+      start_date: start,
+      datetime_resolution: 'hour',
+    }).then(function(response) {
+      vm.processData(response.data);
+    });
+  }
+
+  vm.loadData();
 }
 
 
