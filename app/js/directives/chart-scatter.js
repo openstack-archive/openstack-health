bc14497053a97f636bfada428c8d865c74493646
Matthew Treinish <mtreinish@kortar.org>
2016-01-08 00:46:12 -0500

    Switch to a scatter plot for test runtime
    This commit converts the runtime graph on the per test page to use
    a scatter plot instead of a line chart. Since there are often large
    discontinuities in the graph and the data is inherently noisy a line
    plot can be problematic. Using a scatter plot will be more
    representative of what we're trying to accomplish with the plot.
    As part of this effort rows points with NaN for the value, mean, and
    std dev are all dropped from the api response.
    
    Change-Id: I87baf1ce98b4729702045a8e0046cbc49f372a0e

---
+++ app/js/directives/chart-scatter.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var directivesModule = require('./_index.js');
+
+var d3 = require('d3');
+var nv = require('nvd3');
+
+/**
+ * @ngInject
+ */
+function chartScatter() {
+  var link = function(scope, el, attrs) {
+    scope.$on("loading-started", function() {
+      el.css({"display" : "none"});
+    });
+
+    scope.$on("loading-complete", function() {
+      el.css({"display" : "block"});
+    });
+
+    var chart = null;
+
+    var svg = d3.select(el[0]).append('svg')
+    .attr('width', attrs.width)
+    .attr('height', attrs.height);
+
+    var update = function(data) {
+      if (typeof data === "undefined") {
+        return;
+      }
+
+      chart = nv.models.scatterChart()
+        .duration(300);
+
+      chart.xAxis.tickFormat(function(d) { return d3.time.format("%x")(new Date(d)); });
+      if (attrs.forceY) {
+        chart.forceY(angular.fromJson(attrs.forceY));
+      }
+      if (attrs.tickFormatX) {
+        chart.yAxis.tickFormat(d3.format(attrs.tickFormatX));
+      }
+      if (attrs.tickFormatY) {
+        chart.yAxis.tickFormat(d3.format(attrs.tickFormatY));
+      }
+
+      svg.datum(data).call(chart);
+    };
+
+    scope.$on('windowResize', function() {
+      if (chart !== null) {
+        chart.update();
+      }
+    });
+
+    scope.$watch('data', update);
+  };
+
+  return {
+    restrict: 'EA',
+    scope: {
+      'data': '=',
+      'width': '@',
+      'height': '@'
+    },
+    link: link
+  };
+}
+
+directivesModule.directive('chartScatter', chartScatter);
