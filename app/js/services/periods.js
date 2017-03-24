4af48d0971dd627995b280f9b96fe5828045589c
Masayuki Igawa <masayuki@igawa.me>
2017-03-24 12:43:48 +0900

    Change default period of all graphs to 1 week
    This commit changes default period of all graphs to 1 week except
    grouped-runs which is already set as 2 weeks. We already changed it in
    the home page[1] and it seems good. So let's make it the others.
    
    [1] Ia8cdf52d191453c9bab59669550c0d7dc8faa6e2
    
    Change-Id: Idaec6617032c48c64aa7e860bc95a7556e43f1c9

--- app/js/services/periods.js
+++ app/js/services/periods.js
@@ -31,7 +31,7 @@ var periodsService = function() {
               'preference': {'hours': 12}},
       'hour': {'min':       {'hours': 12},
                'max':       {'months': 1},
-               'preference': {'months': 1}},
+               'preference': {'weeks': 1}},
       'day': {'min':        {'hours': 48},
               'max':        null,
               'preference': {'months': 3}}
@@ -45,7 +45,7 @@ var periodsService = function() {
               'preference': {'hours': 12}},
       'hour': {'min':       {'hours': 12},
                'max':       {'months': 3},
-               'preference': {'months': 1}},
+               'preference': {'weeks': 1}},
       'day': {'min':        {'hours': 48},
               'max':        null,
               'preference': {'months': 3}}
