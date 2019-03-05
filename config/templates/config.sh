b14ba09bc52ea68697fa98c5810f6900eab6d318
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-03-05 12:36:52 +0000

    Add a github event source and channel

---
+++ config/templates/config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Replace __TOKENS__ in files with environment variables
+# Use like this:
+#  cat config/templates/githubsecret.yaml | config/templates/config.sh | kubectl apply -f -
+sed -e 's/__ACCESS_TOKEN__/'$GITHUB_ACCESS_TOKEN'/g' \
+    -e 's/__SECRET_TOKEN__/'$GITHUB_SECRET_TOKEN'/g'
