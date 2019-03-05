a8a3118f726aeaf7a31332c3b3bb2894ddff44b4
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-03-05 23:39:02 +0000

    Add ingress for the github event source

--- config/templates/config.sh
+++ config/templates/config.sh
@@ -4,4 +4,6 @@
 # Use like this:
 #  cat config/templates/githubsecret.yaml | config/templates/config.sh | kubectl apply -f -
 sed -e 's/__ACCESS_TOKEN__/'$GITHUB_ACCESS_TOKEN'/g' \
-    -e 's/__SECRET_TOKEN__/'$GITHUB_SECRET_TOKEN'/g'
+    -e 's/__SECRET_TOKEN__/'$GITHUB_SECRET_TOKEN'/g' \
+    -e 's/__GITHUBSOURCE_ROUTE_NAME__/'$GITHUBSOURCE_ROUTE_NAME'/g' \
+    -e 's/__GITHUBSOURCE_ROUTE_DOMAIN__/'$GITHUBSOURCE_ROUTE_DOMAIN'/g'
